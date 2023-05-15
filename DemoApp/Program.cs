8819f2a6ea37196c23bc8d07ff5e6f7a9c8430d1
Long <ntlong210820@gmail.com>
2023-05-16 00:30:42 +0700

    rewrite code

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -3,85 +3,195 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;  // include the System.IO namespace
 
 namespace DemoApp
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   //                      0                   1
+        {
             Mega();
-
+            //Power();
         }
         static void Mega()
         {
-            string[,] Mega = {
-                    { "01", "02", "03", "08", "05", "06" }, //[0, 0]
-                    { "07", "08", "09", "10", "11", "12" }, //[1, 0]
-                    { "13", "14", "15", "16", "17", "18" },
-                    { "19", "02", "06", "25", "23", "30" },    //[2, 0]
-                    { "25", "26", "27", "28", "29", "30" }
-            };
-            string[,] Megafetch = {
-                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }, //[0, 0]
-                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }, //[1, 0]
-                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
-                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
-                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }//[2, 0]
-            };
-            int row = 0;
-            for (int i = 0; i < Mega.GetLength(0); i++)
+            // Specify a name for your top-level folder.
+            string path = @"C:\tmp\MyTest.txt"; string data = @"C:\tmp\data.txt"; string Sum = @"C:\tmp\sum.txt"; string inputmega = @"C:\tmp\inputmega.txt";
+            using (StreamWriter sw = File.AppendText(path))
             {
-                for (int j = 0; j < Mega.GetLength(1); j++)
+                sw.WriteLine("mega--------------------------->"+ DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+            }
+            // Read data from file into megaNumbers array
+            string[,] megaNumbers = new string[10, 6];
+            string[] lines = File.ReadAllLines(inputmega);
+            for (int i = 0; i < megaNumbers.GetLength(0); i++)
+            {
+                string[] numbers = lines[i].Split(' ');
+                for (int j = 0; j < megaNumbers.GetLength(1); j++)
                 {
-                    for (int e = j + 1; e < Mega.GetLength(1); e++)
+                    megaNumbers[i, j] = numbers[j].Trim();
+                }
+            }
+            string[,] megaFetch = new string[10, 15]; // Increase size if needed
+            int row = 0;
+
+            for (int i = 0; i < megaNumbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < megaNumbers.GetLength(1); j++)
+                {
+                    using (StreamWriter sw = File.AppendText(data))
                     {
-                        Console.Write(Mega[i, j] + Mega[i, e] + "     ");
-                        Megafetch[i, row] = Mega[i, j] + Mega[i, e];
+                        sw.WriteLine("INSERT INTO Mega VALUES(N'" + megaNumbers[i, j] + "'); ");
+                    }
+                    for (int e = j + 1; e < megaNumbers.GetLength(1); e++)
+                    {
+                        //Console.Write(Mega[i, j] + Mega[i, e] + "     ");
+                        megaFetch[i, row] = megaNumbers[i, j] + megaNumbers[i, e];
                         if (row < 14) row++;
                     }
-                    Console.WriteLine();
-
+                    //Console.WriteLine();
                 }
                 row = 0;
                 Console.WriteLine();
             }
             //printf Megafetch
-
+            /*
             for (int i = 0; i < Megafetch.GetLength(0); i++)
             {
                 for (int j = 0; j < Megafetch.GetLength(1); j++)
                 {
-                    Console.Write(Megafetch[i, j] + "   ");
+                    //Console.Write(Megafetch[i, j] + "   ");
                 }
-                Console.WriteLine();
-            }
+                //Console.WriteLine();
+            }*/
 
-            for (int i = 0; i < Megafetch.GetLength(0); i++)
+            for (int i = 0; i < megaFetch.GetLength(0); i++)
             {
-                for (int j = 0; j < Megafetch.GetLength(1); j++)
+                for (int j = 0; j < megaFetch.GetLength(1); j++)
                 {
-                    for (int e = i + 1; e < Megafetch.GetLength(0); e++)
+                    for (int e = i + 1; e < megaFetch.GetLength(0); e++)
                     {
-                        for (int f = 0; f < Megafetch.GetLength(1); f++)
+                        for (int f = 0; f < megaFetch.GetLength(1); f++)
                         {
-                            //Console.Write(Megafetch[i, j] + " " + Megafetch[e, f] + "     ");
-                            if (Megafetch[i, j] == Megafetch[e, f])
+                            using (StreamWriter sw = File.AppendText(Sum))
                             {
-                                Console.Write(Megafetch[i, j] + " " + Megafetch[e, f] + "     ");
+                                sw.WriteLine("INSERT INTO MegaSum VALUES(N'" + megaFetch[i, j] + megaFetch[e, f] + "'); ");
+                            }
+                            //Console.Write(Megafetch[i, j] + " " + Megafetch[e, f] + "     ");
+                            if (megaFetch[i, j] == megaFetch[e, f])
+                            {
+                                Console.Write(megaFetch[i, j] + " " + megaFetch[e, f] + "     ");
+                                using (StreamWriter sw = File.AppendText(path))
+                                {
+                                    sw.WriteLine(megaFetch[i, j] + " " + megaFetch[e, f]);
+                                }
                             }
                             //Console.Write(Megafetch[i, j] + " " + Megafetch[e, f] + "     ");
                         }
-                        Console.WriteLine();
+                        //Console.WriteLine();
                         //Console.Write(Megafetch[i, j] + " " + Megafetch[e, j] + "     ");
                     }
-                    Console.WriteLine();
+                    //Console.WriteLine();
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Power()
+        {
+            // Specify a name for your top-level folder.
+            string path = @"C:\tmp\MyTest.txt"; string data = @"C:\tmp\data.txt"; string Sum = @"C:\tmp\sum.txt"; //string inputPower = @"C:\tmp\inputpower.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("Power--------------------------->" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
             }
 
+            string[,] Power = {
+                    { "02", "13", "16", "18", "41", "42" }, //#00808
+                    { "08", "23", "24", "48", "49", "52" }, //#00807
+                    { "04", "06", "18", "27", "52", "53" }, //#00806
+                    { "03", "09", "21", "22", "26", "35" }, //#00805
+                    { "03", "15", "22", "39", "46", "48" }, //#00804
+                    { "01", "19", "25", "27", "38", "42" }, //#00803
+                    { "04", "20", "26", "36", "47", "51" }, //#00802
 
 
+            };
+            string[,] Powerfetch = {
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }, //[0, 0]
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }, //[1, 0]
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
+                    { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" }
+
+            };
+            int row = 0;
+            for (int i = 0; i < Power.GetLength(0); i++)
+            {
+                for (int j = 0; j < Power.GetLength(1); j++)
+                {
+                    using (StreamWriter sw = File.AppendText(data))
+                    {
+                        sw.WriteLine("INSERT INTO Power VALUES(N'" + Power[i, j]+"'); ");
+                    }
+
+                    for (int e = j + 1; e < Power.GetLength(1); e++)
+                    {
+                        //Console.Write(Power[i, j] + Power[i, e] + "     ");
+                        Powerfetch[i, row] = Power[i, j] + Power[i, e];
+                        if (row < 20) row++;
+                    }
+                    //Console.WriteLine();
+
+                }
+                row = 0;
+                //Console.WriteLine();
+            }
+            //printf Powerfetch
+            /*
+            for (int i = 0; i < Powerfetch.GetLength(0); i++)
+            {
+                for (int j = 0; j < Powerfetch.GetLength(1); j++)
+                {
+                    //Console.Write(Powerfetch[i, j] + "   ");
+                }
+                //Console.WriteLine();
+            }*/
+
+            for (int i = 0; i < Powerfetch.GetLength(0); i++)
+            {
+                for (int j = 0; j < Powerfetch.GetLength(1); j++)
+                {
+                    for (int e = i + 1; e < Powerfetch.GetLength(0); e++)
+                    {
+                        for (int f = 0; f < Powerfetch.GetLength(1); f++)
+                        {
+                            using (StreamWriter sw = File.AppendText(Sum))
+                            {
+                                sw.WriteLine("INSERT INTO PowerSum VALUES(N'" + Powerfetch[i, j] + Powerfetch[e, f] + "'); ");
+                            }
+                            Console.Write(Powerfetch[i, j] + " " + Powerfetch[e, f] + "     ");
+                            if (Powerfetch[i, j] == Powerfetch[e, f])
+                            {
+                                //Console.Write(Powerfetch[i, j] + " " + Powerfetch[e, f] + "     ");
+                                using (StreamWriter sw = File.AppendText(path))
+                                {
+                                    sw.WriteLine(Powerfetch[i, j] + " " + Powerfetch[e, f]);
+                                }
+                            }
+                            //Console.Write(Powerfetch[i, j] + " " + Powerfetch[e, f] + "     ");
+                        }
+                        //Console.WriteLine();
+                        //Console.Write(Megafetch[i, j] + " " + Megafetch[e, j] + "     ");
+                    }
+                    //Console.WriteLine();
+                }
+                //Console.WriteLine();
+            }
             Console.ReadKey();
         }
 
